75be84bdf2a90067ae144e05ff04d82b2957ef3f
milkiwayRN <saw4enk0@bk.ru>
2017-11-21 02:53:03 +0300

    new commit

--- msas.m
+++ msas.m
@@ -25,7 +25,7 @@ trash = 0;
 for i = 1:size(D,1)
     for j = 1:size(D,2)
         if D(i,j) ~=0
-            [shortestPath,trash] = Find_best_road( i,j,E,A,C,func,D(i,j),[i],T );
+            [shortestPath,trash] = Find_best_road( i,j,E,A,C,func,D(i,j),[i],zeros(size(E,1)) );
             for z = 1:(length(shortestPath)-1)
                 T(shortestPath(z),shortestPath(z + 1)) = T(shortestPath(z),shortestPath(z + 1)) + D(i,j);
             end
@@ -33,14 +33,14 @@ for i = 1:size(D,1)
     end
 end
 %end of first iteration
-epsilon = 10;
+epsilon = inf;
 iteration = 0;
 int_func = int(func,x,0,x);
 int_func(x,a,c) = int_func;
 x0 = T(:);
 while (epsilon > 1) && (iteration < 10) 
     Y = zeros(size(E,1));
-    for i = 1:size(D,1)
+    for i = 1:size(D,1)%составляем кратчайшие пути во взвешенном графе
         for j = 1:size(D,2)
             if D(i,j) ~=0
                 [shortestPath,trash] = Find_best_road( i,j,E,A,C,func,D(i,j),[i],T );
@@ -50,9 +50,12 @@ while (epsilon > 1) && (iteration < 10)
             end
         end
     end
+    % теперь находим направление смещения - d и получаем функцию G для
+    % минимизации
     y = Y(:);
     d = y - x0;
     G = get_function_G_for_optimize( A,C,int_func,x0,d,alpha );
+    %минимизация функции G и нахождение нового ответа - x0
     alpha_fl = fminbnd(G,0,1);
     y = x0;
     x0 = x0 + d * alpha_fl;
