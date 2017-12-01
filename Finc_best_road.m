d9625e1f52376d23dca9bf55666e63f5f5331779
nikitalpopov <tapochek97@ya.ru>
2017-12-02 00:39:58 +0300

    uglify

--- Finc_best_road.m
+++ Finc_best_road.m
@@ -1,12 +1,12 @@
-function [ path, cost ] = Finc_best_road(r,s,E,A,C,func,price,T)
-    Temp = zeros(size(T,1));
-    for i = 1:size(T,1)
-        for j = 1:size(T,2)
-            if E(i,j)~=0
-                Temp(i,j)=func(T(i,j)+price,A(i,j),C(i,j));
+function [ path, cost ] = Finc_best_road(r, s, E, A, C, func, price, T)
+    Temp = zeros(size(T, 1));
+    for i = 1 : size(T, 1)
+        for j = 1 : size(T, 2)
+            if E(i, j) ~= 0
+                Temp(i, j) = func(T(i, j) + price, A(i, j), C(i, j));
             end
         end
     end
-    [cost path] = dijkstra(Temp,r,s);
-    path = path(end:-1:1);
+    [cost path] = dijkstra(Temp, r, s);
+    path = path(end : -1 : 1);
 end
